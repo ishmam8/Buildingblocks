3a65aef1e304e8d9896747d8dc3e37086f22cc4e
ahmedbil.157@gmail.com <72715510+ahmedbil@users.noreply.github.com>
2021-06-13 19:14:31 -0400

    Implemented email notifications for new batch insert

--- backend/server.js
+++ backend/server.js
@@ -21,7 +21,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}))
 
 const uri = process.env.ATLAS_URI;
-mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true }
+mongoose.connect(uri, { useNewUrlParser: true,  useUnifiedTopology: true, useCreateIndex: true }
 );
 
 const connection = mongoose.connection;
