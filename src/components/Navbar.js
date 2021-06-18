7263f58bce4ede68f984f91f7aa5c9ea8ed97557
GrantPau <GrantPau@github.com>
2021-06-17 21:13:20 -0700

    Added navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+
+//Styles import
+import "./css/Navbar.css";
+
+// Images import
+//import logo from "../images/logo.svg";
+import logo from "../images/laptop.png";
+//import bars from "../images/icons/menuBars.svg";
+
+function Navbar() {
+  const [showLinks, setShowLinks] = useState(false);
+  return (
+    <div className="navbarContainer">
+      <div className="navbarLeft">
+        <img className="logo" src={logo} alt="Welcome to Building Blocks!" />
+      </div>
+
+      <div className="navbarRight">
+        <div className="navbarLinks">
+          <a className="blackNavLinks" href="/">
+            About Us
+          </a>
+          <a className="blackNavLinks" href="/team">
+            Our Team
+          </a>
+          <a className="blackNavLinks" href="/">
+            Updates
+          </a>
+          <a className="blackNavLinks" href="/">
+            Contact
+          </a>
+
+          <a href="/login" className="blueNavLinks">
+            Login
+          </a>
+          <a href="/signup" className="blueNavLinks">
+            Sign Up{" "}
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
