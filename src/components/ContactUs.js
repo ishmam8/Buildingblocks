3b7ab4135905c4d1aa8c4b76405f05c89370ad45
Eros <elewites@gmail.com>
2021-06-17 18:48:16 -0700

    add styling to contact us page

--- src/components/ContactUs.js
+++ src/components/ContactUs.js
@@ -6,6 +6,7 @@ import Footer from "./Footer";
 
 //Style Imports
 import "./css/ContactUs.css";
+import ContactImage from "../images/contact-us-image.svg";
 
 export default function ContactUs() {
   return (
@@ -16,10 +17,15 @@ export default function ContactUs() {
         <div className="contact-us-text">
           If your student group wants to use Building Blocks for your peer
           mentoring program, or you’d like to get invovled with Building Blocks,
-          shoot us an email! We’ll do our best to respond in one business day.
+          shoot us an email!
+          <br />
+          <br />
+          We’ll do our best to respond in one business day.
         </div>
         <div className="contact-us-email">buildingblocksapp@gmail.com</div>
-        <div className="contact-us-image"></div>
+        <div className="contact-us-image">
+          <img src={ContactImage} alt="icon" />
+        </div>
       </div>
     </div>
   );
