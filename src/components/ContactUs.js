938506c2f2c6064d8a8417b0c2a2d4652c5bf083
Eros <elewites@gmail.com>
2021-06-18 17:15:22 -0700

    finish responsiveness for contact-us page

--- src/components/ContactUs.js
+++ src/components/ContactUs.js
@@ -12,19 +12,20 @@ export default function ContactUs() {
   return (
     <div className="contact-us-body">
       <Navbar />
+      <div className="contact-us-banner">Contact Us</div>
       <div className="contact-us-container">
-        <div className="contact-us-banner">Contact Us</div>
         <div className="contact-us-text">
           If your student group wants to use Building Blocks for your peer
           mentoring program, or you’d like to get invovled with Building Blocks,
           shoot us an email!
-          <br /> <br />
+        </div>
+        <div className="contact-us-lower-text">
           We’ll do our best to respond in one business day.
         </div>
         <div className="contact-us-email">buildingblocksapp@gmail.com</div>
-        <div className="contact-us-image">
-          <img src={ContactImage} alt="icon" />
-        </div>
+      </div>
+      <div className="contact-us-image">
+        <img src={ContactImage} alt="icon" />
       </div>
     </div>
   );
