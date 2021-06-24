0e848b92815917908b3ba0bb0010957c918132b1
GrantPau <GrantPau@github.com>
2021-06-23 19:28:32 -0700

    worked on signup.js

--- src/components/GoogleButton.js
+++ src/components/GoogleButton.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import GoogleLogin from 'react-google-login'
 import axios from 'axios'
+import './css/Signup.css';
+import GoogleIcon from "../images/new-google-favicon-512.png"
 export class GoogleButton extends Component {
     responseSuccessGoogle=(response)=>{
         console.log(response);
@@ -22,6 +24,12 @@ export class GoogleButton extends Component {
             <div>
                 <GoogleLogin 
                 clientId="617015858222-1ddpao4ifrsv6i6e9430ljrjfe9mnfe0.apps.googleusercontent.com"
+                render={(renderProps) => (
+                    <button className="customGoogleButton" onClick={renderProps.onClick} disabled={renderProps.disabled}>
+                      Sign-up with Google
+                      <img className="buttonIcons" src={GoogleIcon} />
+                    </button>
+                  )}
                 buttonText="Signup with Google"
                 onSuccess={this.responseSuccessGoogle}
                 onFailure={this.responseErrorGoogle}
