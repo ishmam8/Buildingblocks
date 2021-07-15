42f19a59ce6efba7f03a3ca2a4163165b1a9cd33
AlexLassooij <alex.lassooij@icloud.com>
2021-07-14 22:53:28 -0400

    got rid of duplicate navbars

---
+++ src/components/MasterNavbar.js
@@ -0,0 +1,35 @@
+import React from "react";
+import {useSelector, useDispatch} from "react-redux";
+import Navbar from "./Navbar";
+import DashboardNavbar from "./DashboardNavbar";
+
+
+
+export default function MasterNavbar() {
+
+    const dispatch = useDispatch();
+
+    function logout() {
+        dispatch({ type: "CHANGE_USERNAME", username: null });
+        dispatch({ type: "CHANGE_EMAIL", email: null });
+        dispatch({ type: "CHANGE_AVI", avi: null });
+        dispatch({ type: "CHANGE_TYPE", myType: null });
+        dispatch({ type: "CHANGE_CHATROOM", chatroom: false });
+        dispatch({ type: "CHANGE_LASTUSER", lastUser: null });
+        dispatch({ type: "CHANGE_LOGGEDIN", loggedIn: false });
+        window.location.reload();
+    }
+    const loggedIn = useSelector((state) => state.loggedIn);
+
+    if (!loggedIn) {
+        return (
+            <Navbar/>
+        );
+    }
+
+    else {
+        return (
+            <DashboardNavbar logout={logout}/>
+        );
+    }
+}
